d309ef669d56d9f4b1b1c5e46f5f6639a2308ad1
Xiuwen Zheng <zhengxwen@gmail.com>
2017-05-17 00:20:43 -0700

    typo

--- src/vectorization.c
+++ src/vectorization.c
@@ -4,19 +4,19 @@
 //
 // Copyright (C) 2016-2017    Xiuwen Zheng
 //
-// This file is part of SeqArray.
+// This file is part of PySeqArray.
 //
-// SeqArray is free software: you can redistribute it and/or modify it
+// PySeqArray is free software: you can redistribute it and/or modify it
 // under the terms of the GNU General Public License Version 3 as
 // published by the Free Software Foundation.
 //
-// SeqArray is distributed in the hope that it will be useful, but
+// PySeqArray is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public
-// License along with SeqArray.
+// License along with PySeqArray.
 // If not, see <http://www.gnu.org/licenses/>.
 
 /**
